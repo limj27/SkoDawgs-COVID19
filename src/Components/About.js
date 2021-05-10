eca0b6431b1f542e4deac407f3d0a329d5e08bd3
justin <limjustin2@gmail.com>
2021-05-10 14:02:45 -0700

    Minor style change

--- src/Components/About.js
+++ src/Components/About.js
@@ -23,6 +23,10 @@ class About extends Component {
             <h2>About Our Sponsor</h2>
 
             <p>Our team has been working with a sponsor, Sarah Schacht who is a data + digital fellow from Georgetown University's Beeck Center. We were matched through University Washington's Information School to work on this project to research into what the challenges of reporting and collecting COVID-19 data. Then we also looked into how we can improve the data management model and the government's data standards.</p>
+            <h2>Project Information</h2>
+            <p>Our team set out to understand barriers to accurate, timely COVID-19 data publishing across the United States. Working with our sponsor Sarah Schacht, we investigated how local governments process and normalize a high volume of data as well as publish information to a variety of stakeholders. For our research, we conducted an ecosystem scan of all 50 US states and key informant interviews to understand what is happening inside public health departments. Using our findings, we produced reports highlighting shortcomings and obstacles in COVID-19 data reporting and provided recommendations. The report we created will hopefully be used to remedy existing problems and better prepare government bodies to collect and report data in future emergency situations.</p>
+            <h2>Project Status</h2>
+            <p>Project has been finished on our part and has been handed off to our sponsor Sarah. The likely chance of another team picking this project up is very unlikely since we finished the project.</p>
               <div className="columns download">
                 <p>
                   <a href={resumeDownload} className="button">
@@ -30,11 +34,7 @@ class About extends Component {
                   </a>
                 </p>
               </div>
-            <br></br>
-            <h2>Project Information</h2>
-            <p>Our team set out to understand barriers to accurate, timely COVID-19 data publishing across the United States. Working with our sponsor Sarah Schacht, we investigated how local governments process and normalize a high volume of data as well as publish information to a variety of stakeholders. For our research, we conducted an ecosystem scan of all 50 US states and key informant interviews to understand what is happening inside public health departments. Using our findings, we produced reports highlighting shortcomings and obstacles in COVID-19 data reporting and provided recommendations. The report we created will hopefully be used to remedy existing problems and better prepare government bodies to collect and report data in future emergency situations.</p>
-            <h2>Project Status</h2>
-            <p>Project has been finished on our part and has been handed off to our sponsor Sarah. The likely chance of another team picking this project up is very unlikely since we finished the project.</p>
+            
             </div>
           </div>
       </section>
